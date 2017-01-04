7eb5d7377b9a2ce0640afc4a8075e38fcc26d519
Thomas Debarre <thomas.debarre@gmail.com>
2017-01-04 15:35:22 +0100

    New dataset

---
+++ project/data/clean_corpus.py
@@ -0,0 +1,25 @@
+import re
+import codecs
+import string
+
+
+fr_input = codecs.open('test.fr', 'r', 'cp1252')
+en_input = codecs.open('test.en', 'r', 'cp1252')
+fr_output = open('clean.fr', 'w')
+en_output = open('clean.en', 'w')
+
+for line in fr_input:
+    translator = str.maketrans({key: None for key in string.punctuation})
+    l = line.translate(translator)
+    l = l.replace("«","")
+    l = l.replace("»","")
+    l = l.lower()
+    l = re.sub(' +', ' ', l)
+    fr_output.write(l)
+
+for line in en_input:
+    translator = str.maketrans({key: None for key in string.punctuation})
+    l = line.translate(translator)
+    l = l.lower()
+    l = re.sub(' +', ' ', l)
+    en_output.write(l)
