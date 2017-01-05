1a8c8458d54902b4546d264a3e531005ba703724
syltruong <syltruong@gmail.com>
2017-01-05 13:51:47 +0100

    created corpus_statistics

--- project/lib/import_corpus.py
+++ project/lib/import_corpus.py
@@ -21,6 +21,21 @@ def import_corpus(path_to_corpus, n_sentences):
 
     return corpus, unique_words
 
+# returns corpus statistics
+def corpus_statistics(corpus):
+    n_senteces = len(corpus)
+    n_words = {} # dictionnary (n_words,n_corresponding_sentences) 
+    for sentence in corpus:
+        n = len(split_sentence(sentence))
+        if n in n_words:
+            n_words[n] += 1
+        else:
+            n_words[n] = 1
+    print "Corpus statistics:"
+    for key in n_words:
+        print "%d words -> %d sentences" % (key,n_words[key])
+    return n_words
+
 
 def import_all(path_to_french, path_to_english, n_sentences):
     fr_corpus, fr_dict = import_corpus(path_to_french, n_sentences)
