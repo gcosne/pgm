5eeb786c7f57ef5b1e2973808f71354deea9bd46
syltruong <syltruong@gmail.com>
2017-01-18 17:29:10 +0100

    ran all models on all sentences

--- project/run_tests.sh
+++ project/run_tests.sh
@@ -5,9 +5,9 @@ main="translate.py"
 #CORPUS_FR="data/corpus_fr.txt"
 #CORPUS_EN="data/corpus_en.txt"
 
-CORPUS_EN="data/clean.fr.12"
-CORPUS_FR="data/clean.en.12"
+CORPUS_EN="data/clean.clean.fr.12"
+CORPUS_FR="data/clean.clean.en.12"
 
-method="2 3"
+method="1 2 3"
 
 python $main $CORPUS_FR $CORPUS_EN -m $method
